74281e040809af6c07319cb253dbe21e8e30bec4
Elizabeth paez <elizabeth.paezramirez@outlook.com>
2020-09-06 20:10:02 -0500

    voltea la palabra

---
+++ src/main/java/prueba.java
@@ -0,0 +1,11 @@
+
+public class prueba {
+
+    public static void main(String[] args) {
+        String arreglo = "hola";
+        char[] cambiazo = arreglo.toCharArray();
+      for (int  i = ((cambiazo.length)-1) ; i >= 0; i--){
+        System.out.print(cambiazo[i]);
+        }
+      }
+    }
